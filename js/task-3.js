bef925712fd3993d0fca111c43545c394eb72891
Maksim Karpenko <ozuzuandvduvatel@gmail.com>
2024-07-12 16:35:58 +0300

    end work task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,22 @@
 'use strict';
+
+
+function filterArray(numbers, value) {
+  let array = [];
+  for (const iterator of numbers) {
+    if (iterator > value) {
+      array.push(iterator);
+    }
+  }
+return array;
+}
+
+
+
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
