19a869e499326487fae01db4ab63375765952a47
Maksim Karpenko <ozuzuandvduvatel@gmail.com>
2024-07-12 16:50:51 +0300

    fix task 3

--- js/task-3.js
+++ js/task-3.js
@@ -3,9 +3,9 @@
 
 function filterArray(numbers, value) {
   let array = [];
-  for (const iterator of numbers) {
-    if (iterator > value) {
-      array.push(iterator);
+  for (let index = 0; index < numbers.length; index++) {
+    if (numbers[index] > value) {
+      array.push(numbers[index]);
     }
   }
   return array;
@@ -20,3 +20,5 @@ console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
 console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
 console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
 console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+
